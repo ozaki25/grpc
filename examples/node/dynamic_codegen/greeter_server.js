fdb67321c4041ef730264548158f891568b3b872
ozaki <fs65ugumori@yahoo.co.jp>
2018-11-14 22:16:17 +0900

    hello

--- examples/node/dynamic_codegen/greeter_server.js
+++ examples/node/dynamic_codegen/greeter_server.js
@@ -37,13 +37,17 @@ function sayHello(call, callback) {
   callback(null, {message: 'Hello ' + call.request.name});
 }
 
+function sayHelloAgain(call, callback) {
+  callback(null, {message: 'Hello again, ' + call.request.name});
+}
+
 /**
  * Starts an RPC server that receives requests for the Greeter service at the
  * sample server port
  */
 function main() {
   var server = new grpc.Server();
-  server.addService(hello_proto.Greeter.service, {sayHello: sayHello});
+  server.addService(hello_proto.Greeter.service, {sayHello, sayHelloAgain});
   server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
   server.start();
 }
